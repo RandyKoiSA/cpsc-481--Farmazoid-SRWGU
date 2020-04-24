4c73b944e47f3379d1b8944d67d0132f0882118c
Randy Le <97randy.le@gmail.com>
2020-04-24 15:55:40 -0700

    create more classes

--- sketch.js
+++ sketch.js
@@ -1,3 +1,49 @@
+/*
+    Class Initialization
+*/
+
+class Plant{
+    // fruit state = seed -> bushed -> blossom -> green-fruit -> red-fruit/harvestable -> dropped
+    // water reserve = stores a max of 3 units anything, above 3 unit of water will damage the plant
+    // plant state = green(health) -> yellow -> brown -> black(died)
+    // each non-rain day will consume one water reserve.
+    constructor() {
+        //assuming there is only one fruit type to make it easier
+        this.plant_state = "green";
+        this.fruit_state = "seed";
+        this.water_reserve = 0;
+    }
+
+}
+
+class Barn{
+    // unlimited seed
+    // unlimited fertilizer
+    // unlimited smudge pots
+    // unlimited soap
+    constructor(row, col){
+        this.water_amount = 0;
+        //this.seed_amount = 0;
+        //this.fertilizer_amount = 0;
+        //this.smudge_pots = 0;
+        //this.soap_amount = 0;
+        this.apple_amount = 0;
+        this.berry_amount = 0;
+        this.corn_amount = 0;
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Farmzoid{
+    constructor(row, col, carried_supplied){
+        this.row = row;
+        this.col = col;
+        this.carried_supplied = carried_supplied;
+    }
+}
+
+// Used to add into the Grid
 class Cell{
     constructor(traversable, tileType, row, col) {
         this.traversable = traversable;
@@ -20,43 +66,52 @@ class Grid {
                 // Tile is grass
                 if(path[row][col] == 0) 
                 {
-                    this.contents[row][col] = new Cell(true, 0, row, col);
+                    this.contents[row][col] = new Cell(true, "grass", row, col);
                 } 
                 // Tile is a barn
                 else if(path[row][col] == 1)
                 {
-                    this.contents[row][col] = new Cell(true, 1, row, col);
+                    barn = new Barn(row, col);
+                    this.contents[row][col] = new Cell(true, "barn", row, col);
                 }
                 // Tile is a river
                 else if(path[row][col] == 2) 
                 { 
-                    this.contents[row][col] = new Cell(false, 2, row, col);
+                    this.contents[row][col] = new Cell(false, "river", row, col);
                 }
                 // Tile is a bridge
                 else if(path[row][col] == 3)
                 {
-                    this.contents[row][col] = new Cell(true, 3, row, col);
+                    this.contents[row][col] = new Cell(true, "bridge", row, col);
                 }
-                // Tile is a mine-area
+                // Tile is a mine
                 else if(path[row][col] == 4)
                 {
-                    this.contents[row][col] = new Cell(false, 4, row, col);
+                    this.contents[row][col] = new Cell(false, "mine", row, col);
+                }
+                else if(path[row][col] == 5)
+                {
+                    this.contents[row][col] = new Cell(true, "grass", row, col);
+                    farmzoids.push(new Farmzoid(row, col));
                 }
             }
         }
     }
 }
 
+/*
+    Variable Initialization
+*/
 // Dimmensions
 let num_rows = 40;
 let num_cols = 40;
 let cell_size = 20;
 
-// Grass = 0
-// Barn = 1
-// River = 2
-// Bridge = 3
-// Mine-Area = 4
+var barn;
+var plants = [];
+var farmzoids = [];
+
+// Grass = 0, Barn = 1, River = 2, Bridge = 3, Mine = 4, Farmzoid = 5
 let path = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -81,7 +136,7 @@ let path = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -102,6 +157,7 @@ let path = [
 
 let grid = new Grid(num_rows, num_cols, path);
 
+
 // Sets up the parameters and logic
 function setup() {
     createCanvas(num_cols*cell_size, num_rows*cell_size);
@@ -115,36 +171,36 @@ function setup() {
     bridge_color = color(136, 140, 141); // stone gray
     barn_color = color(101, 67, 33); // dark brown
     error_color = color(255, 0, 0);
-
+    farmzoid_color = color(255, 255, 255); // white
 
     // Draw the grid
     stroke(outline_color); // set outlines to white
     for(row = 0; row < num_rows; row++) {
         for(col = 0; col < num_cols; col++) {
             current_cell = grid.contents[row][col];
-            if(current_cell.tileType == 0) {
+            if(current_cell.tileType == "grass") {
                 fill(grass_color);
                 square(col*cell_size, row*cell_size, cell_size);
             }
-            if(current_cell.tileType == 1) {
+            if(current_cell.tileType == "barn") {
                 fill(barn_color);
+                barn = new Barn();
                 square(col*cell_size, row*cell_size, cell_size);
             }
-            if(current_cell.tileType == 2) {
+            if(current_cell.tileType == "river") {
                 fill(river_color);
                 square(col*cell_size, row*cell_size, cell_size);
             }
-            if(current_cell.tileType == 3) {
+            if(current_cell.tileType == "bridge") {
                 fill(bridge_color);
                 square(col*cell_size, row*cell_size, cell_size);
             }
-            if(current_cell.tileType == 4) {
+            if(current_cell.tileType == "mine") {
                 fill(mine_area_color);
                 square(col*cell_size, row*cell_size, cell_size);
             } 
         }
     }
-
 }
 
 // Grass = 0
@@ -154,5 +210,12 @@ function setup() {
 // Mine-Area = 4
 // Everything in draw() strictly relates to graphics, not logic
 function draw() { 
-    
+    // draw farmzoid
+    stroke((0, 0, 0));
+    for(i = 0; i < farmzoids.length; i++){
+        fill(farmzoid_color);
+        col = farmzoids[i].col;
+        row = farmzoids[i].row;
+        circle(col*cell_size+(cell_size/2), (row*cell_size)+(cell_size/2), cell_size);
+    }
 } 

1b0d31ca48d2d6160f9455389979ded23c5e3767
Randy Le <97randy.le@gmail.com>
2020-05-01 10:12:37 -0700

    added more enums and can now draw plants now.

--- sketch.js
+++ sketch.js
@@ -7,14 +7,16 @@ class Plant{
     // water reserve = stores a max of 3 units anything, above 3 unit of water will damage the plant
     // plant state = green(health) -> yellow -> brown -> black(died)
     // each non-rain day will consume one water reserve.
-    constructor() {
+    constructor(row, col, plant_type = plantType.Apple) {
         //assuming there is only one fruit type to make it easier
-        this.plant_state = "green";
-        this.fruit_state = "seed";
+        this.plant_type = plant_type;
+        this.plant_state = plantState.Flowering;
         this.water_reserve = 0;
         this.blight = false;
         this.protected_from_cold = false;
         this.cold_issues = false;
+        this.row = row;
+        this.col = col;
     }
 
 }
@@ -116,6 +118,20 @@ const cellType = {
     Farmzoid: 5
 };
 
+const plantType = {
+    Corn: "corn",
+    Apple: "apple",
+    Berry: "berry"
+}
+
+const plantState = {
+    Flowering: "flowering",
+    Green: "green",
+    Red: "red",
+    NONE: "none"
+}
+
+
 // Dimmensions
 let num_rows = 40;
 let num_cols = 40;
@@ -181,7 +197,6 @@ let grid = new Grid(num_rows, num_cols, path);
 
 // Sets up the parameters and logic
 function setup() {
-    print("Set up is only called once");
     createCanvas(num_cols*cell_size, num_rows*cell_size);
     frameRate(3);
     // Colors
@@ -191,21 +206,29 @@ function setup() {
     river_color = color(0, 0, 255); //blue
     bridge_color = color(136, 140, 141); // stone gray
     barn_color = color(101, 67, 33); // dark brown
-    error_color = color(255, 0, 0);
+    //error_color = color(255, 0, 0);
     farmzoid_color = color(255, 255, 255); // white
+    flowering_color = color(219, 232, 59);
+    green_plant_color = color(32, 183, 31);
+    red_plant_color = color (255, 0, 0);
+    none_plant_color = color(0, 0, 0);
+
+    temp_plant = new Plant(5, 5);
+    plants.push(temp_plant);
 }
 
 function draw() { 
     background(0);
-    // Draw the Grid: River, Grass, Bridge, Barn, Mine
+    /* Draw the Grid: River, Grass, Bridge, Barn, Mine */
     DrawGrid();
+    DrawPlants();
+    /* Ideally, Nature, FSM, then zoids run */
     CheckIfNewDay();
-    // draw farmzoid
+    /* Draw farm zoids */
     MoveFarmzoids();
     DrawFarmzoids();
 
     move_counter += 1;
-
 } 
 
 function CheckIfNewDay()
@@ -215,6 +238,7 @@ function CheckIfNewDay()
         day_counter += 1;
         move_counter = 0;
         ChangeWeather();
+        Run_FMS();
     }
 }
 
@@ -309,6 +333,13 @@ function ChangeWeather()
     }
 }
 
+/* This will check what state the weather it is in. FMS can learn
+what plant needs by running a decision tree, to classify the plant's current needs. The classification
+should then be used to run a rule to determine what tasks need to be done for that plant */
+function Run_FMS()
+{
+
+}
 /* Move Farmzoids will hold D-TREE logic */
 function MoveFarmzoids()
 {
@@ -316,6 +347,10 @@ function MoveFarmzoids()
     {
         // D-TREE logic can go here
         // Check farmzoids surroundings
+        switch(farmzoids[i].carried_supplied)
+        {
+            case carriedSp
+        }
         // Depending on what farmzoid is carrying it will find the plant that needs attention.
         farmzoids[i].col += 1;
     }
@@ -330,6 +365,32 @@ function DrawFarmzoids()
         col = farmzoids[i].col;
         row = farmzoids[i].row;
         circle(col*cell_size+(cell_size/2), (row*cell_size)+(cell_size/2), cell_size);
+    }
+}
+
+/* Draw all existing plants */
+function DrawPlants()
+{
+    for(i = 0; i < plants.length; i++)
+    {
+        col = plants[i].col;
+        row = plants[i].row;
+        switch(plants[i].plant_state)
+        {
+            case plantState.Flowering:
+                fill(flowering_color);
+                break;
+            case plantState.Green:
+                fill(green_plant_color);
+                break;
+            case plantState.Red:
+                fill(red_plant_color);
+                break;
+            case plantState.NONE:
+                fill(none_plant_color);
+                break;
+        }
+        circle(col*cell_size+(cell_size/2), (row*cell_size)+(cell_size/2), cell_size);    
     }
 }
 
@@ -365,12 +426,3 @@ function DrawGrid()
         }
     }
 }
-
-/*
-Move each farmzoids (x40)
-if moved 40 times:
-    increment the day
-    change weather type
-    depending on weather type update the plants status
-    reset the move amount
-*/
